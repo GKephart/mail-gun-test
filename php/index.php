39bc10979e89724235fb31a8beb5b7640cf0aed1
george <gkephart@cnm.edu>
2019-06-20 10:00:25 -0600

    #shipit

--- php/index.php
+++ php/index.php
@@ -47,13 +47,12 @@ try {
 // Now, compose and send your message.
 // $mg->messages()->send($domain, $params);
 	$mg->messages()->send($mailgunDomain, [
-		'from'    => 'g.e.kephart@gmail.com',
-		'to'      => 'gkephart@cnm.edu',
-		'subject' => 'The PHP SDK is awesome!',
-		'text'    => 'It is so simple to send a message.'
+		'from'    => $email,
+		'to'      => $MAIL_RECIPIENTS["email"],
+		'subject' => $subject,
+		'text'    => $message
 	]);
 
-	var_dump($mg);
 
 	// report a successful send!
 	echo "<div class=\"alert alert-success\" role=\"alert\">Email successfully sent.</div>";
